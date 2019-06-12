4081c3e5bc5e73d9a15f15be42de6d2a88ea4260
Jure Zakotnik <jzakotnik@macbook.fritz.box>
2019-06-12 08:03:32 +0200

    added boot scripts

---
+++ scripts/startDashboard.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+sleep 20
+cd /home/pi/optimat
+source optimat_pythonenv/bin/activate
+cd dashboard
+python3 frontend.py
+exit 0
+
---
+++ scripts/startOptimat.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+sleep 20
+cd /home/pi/optimat
+source optimat_pythonenv/bin/activate
+python3 run.py
+exit 0
+
