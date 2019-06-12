9adb957075cbb394cb2c499e0e3622b46b369400
Jure Zakotnik <jzakotnik@macbook.fritz.box>
2019-06-12 23:00:29 +0200

    Fixed error in scheduler

--- run.py
+++ run.py
@@ -14,22 +14,16 @@ logging.basicConfig(filename='optimat.log', level=logging.INFO)
 config = ConfigParser()
 config.read('config.ini')
 
+# this is the handler to answer incoming chat requests
+p = parser.Parser()
+
 
 updater = Updater(token=config.get(
     'main', 'TELEGRAM_BOT_TOKEN'), use_context=True)
 scheduler = updater.job_queue
-
 dispatcher = updater.dispatcher
-p = parser.Parser()
 
-
-def callback_updateDashboard(context: telegram.ext.CallbackContext):
-    p.updateDashboard()
-    #print("Scheduler executed")
-
-
-job = scheduler.run_repeating(
-    callback_updateDashboard, interval=300, first=1)
+# this handler reacts on messages sent to the bot
 
 
 def optimatHandler(update, context):
@@ -41,32 +35,34 @@ def optimatHandler(update, context):
 optimat = MessageHandler(Filters.text, optimatHandler)
 dispatcher.add_handler(optimat)
 
-updater.start_polling()
+# this handler is triggered via scheduler every 5min to update the dashboard in the kitchen
 
 
-def timeInRange(start, end, x):
-    """Return true if x is in the range [start, end]"""
-    if start <= end:
-        return start <= x <= end
-    else:
-        return start <= x or x <= end
+def callback_updateDashboard(context: telegram.ext.CallbackContext):
+    try:
+        p.updateDashboard()
+    except Exception:
+        logging.exception("Update of the dashboard server failed")
 
 
-def checkScheduler():
-    # TODO this is all too harcoded, refactor
-    # TODO This doesn't work with the telegram.ext scheduler, since it blocks
-    # send Traffic at 8:00
-    if timeInRange(
-            datetime.time(7, 59, 55), datetime.time(7, 59, 57),
-            datetime.datetime.now().time()):
-        result = p.parseInput('verkehr kita')
-        updater.bot.sendMessage(config.get(
-            'main', 'TELEGRAM_BOT_TOKEN'), result['reply'])
+dashboardUpdateJob = scheduler.run_repeating(
+    callback_updateDashboard, interval=300, first=1)
 
-    if timeInRange(
-        # TODO fix the hardcoded message for the sbahn notification (is it too late?)
-            datetime.time(17, 15, 55), datetime.time(17, 15, 57),
-            datetime.datetime.now().time()):
+# scheduled chats, e.g. send traffic in the morning or the sbahn in the evening after work
+
+
+def callback_sendTrain(context: telegram.ext.CallbackContext):
+    try:
         result = p.parseInput('sbahn')
         updater.bot.sendMessage(config.get(
             'main', 'TELEGRAM_BOT_TOKEN'), result['reply'])
+    except Exception:
+        logging.exception("Scheduled train info could not be sent")
+
+
+TrainUpdateJob = scheduler.run_daily(
+    callback_sendTrain, datetime.time(hour=17, minute=20, second=0))
+
+
+# This checks for new messages
+updater.start_polling()
--- scripts/startOptimat.sh
+++ scripts/startOptimat.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 sleep 20
 cd /home/pi/optimat
-source optimat_pythonenv/bin/activate
+source env_optimat/bin/activate
 python3 run.py
 exit 0
 
