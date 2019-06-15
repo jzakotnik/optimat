24364367ce7815a3e9d3b7890a86ea3048ae5338
Jure Zakotnik <jzakotnik@macbook.fritz.box>
2019-06-15 16:55:15 +0200

    added exception handling for alarm videos

--- sendNotificationEmail.py
+++ sendNotificationEmail.py
@@ -14,32 +14,36 @@ from email.mime.text import MIMEText
 config = ConfigParser()
 config.read('config.ini')
 
-mail_user = self.config.get('main', 'MAIL_SENDER')
-mail_pwd = self.config.get('main', 'MAIL_PASSWORD')
-mail_to = self.config.get('main', 'MAIL_RECIPIENT')
-MAIL_SERVER = self.config.get('main', 'MAIL_SERVER')
+logging.basicConfig(filename='optimat_alarm.log', level=logging.INFO)
 
-msg = MIMEMultipart()
-mp4files = [f for f in listdir('alarmimages/') if f.endswith('.mp4')]
-msg['From'] = mail_user
-msg['To'] = mail_to
-msg['Subject'] = 'Alarm, Bewegung detektiert!'
-msg.attach(MIMEText("Hello Master! Hier das File.."))
+try:
+    mail_user = self.config.get('main', 'MAIL_SENDER')
+    mail_pwd = self.config.get('main', 'MAIL_PASSWORD')
+    mail_to = self.config.get('main', 'MAIL_RECIPIENT')
+    MAIL_SERVER = self.config.get('main', 'MAIL_SERVER')
 
-part = MIMEBase('application', 'octet-stream')
+    msg = MIMEMultipart()
+    mp4files = [f for f in listdir('alarmimages/') if f.endswith('.mp4')]
+    msg['From'] = mail_user
+    msg['To'] = mail_to
+    msg['Subject'] = 'Alarm, Bewegung detektiert!'
+    msg.attach(MIMEText("Hello Master! Hier das File.."))
 
+    part = MIMEBase('application', 'octet-stream')
 
-part.set_payload(open('alarmimages/'+mp4files[0], 'rb').read())
-Encoders.encode_base64(part)
-part.add_header('Content-Disposition', 'attachment; filename="%s"' %
-                os.path.basename('alarmimages/'+mp4files[0]))
-msg.attach(part)
+    part.set_payload(open('alarmimages/'+mp4files[0], 'rb').read())
+    Encoders.encode_base64(part)
+    part.add_header('Content-Disposition', 'attachment; filename="%s"' %
+                    os.path.basename('alarmimages/'+mp4files[0]))
+    msg.attach(part)
 
-mailServer = smtplib.SMTP(MAIL_SERVER, 587)
-mailServer.ehlo()
-mailServer.starttls()
-mailServer.ehlo()
-mailServer.login(mail_user, mail_pwd)
-mailServer.sendmail(mail_user, mail_to, msg.as_string())
-# Should be mailServer.quit(), but that crashes...
-mailServer.close()
+    mailServer = smtplib.SMTP(MAIL_SERVER, 587)
+    mailServer.ehlo()
+    mailServer.starttls()
+    mailServer.ehlo()
+    mailServer.login(mail_user, mail_pwd)
+    mailServer.sendmail(mail_user, mail_to, msg.as_string())
+    # Should be mailServer.quit(), but that crashes...
+    mailServer.close()
+except Exception:
+    logging.exception("Could not send alarm email video")
