0a4aa2b3a9a8fa5012eed53ab4ae45aa6da49e8e
jzakotnik <jure@zakotnik.de>
2021-04-21 22:48:59 +0200

    upgraded dependencies

--- scripts/startDashboard.sh
+++ scripts/startDashboard.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sleep 20
 cd /home/pi/optimat
-source optimat_pythonenv/bin/activate
+source env_optimat2/bin/activate
 cd dashboard
 python3 frontend.py
 exit 0
--- scripts/startOptimat.sh
+++ scripts/startOptimat.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sleep 20
 cd /home/pi/optimat
-source env_optimat/bin/activate
+source env_optimat2/bin/activate
 python3 run.py
 exit 0
 
